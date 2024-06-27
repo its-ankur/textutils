a2b381324a1093131c357542a2d0d667a90a8cbf
its-ankur <ankuratul2007@gmail.com>
2024-06-27 18:13:02 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Ankur";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <img src={logo} alt="logo" />
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with ankur
-        </a>
-      </header>
-    </div>
+        </div>
+    </>
   );
 }
 
